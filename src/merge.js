8b62a66afb2bb549340066f04bba01bbfa59c8cd
Duknam Yoo <yoo2001818@gmail.com>
2017-02-21 23:29:03 +0900

    Add basic pseudocode to merge function

--- src/merge.js
+++ src/merge.js
@@ -1,9 +1,24 @@
 // @flow
 import type { Branch } from './branch';
+import findMutualParent from './branch/findMutualParent';
 
 export default function merge(merger: Branch, mergee: Branch,
-  conflictDetector: Function, orderer: Function,
+  conflictDetector: Function, orderer: (mergee: Branch,
+    transaction: Transaction) => Number,
 ): Branch {
-  // No-op for now
-  return merger;
+  // Slice the merger and mergee to the mutual parent.
+  let mutualIndex = findMutualParent(merger, mergee);
+  let mergerTemp = merger.slice(mutualIndex + 1);
+  // Create some kind of linked list. We need to create reverse index to
+  // preserve the parent links while inserting transactions.
+  let mergeeTemp = mergee.slice(mutualIndex + 1);
+  // Read each transaction from merger and push into mergee
+  mergerTemp.forEach(transaction => {
+    let position = orderer(mergeeTemp, transaction);
+    // TODO Push it to mergee, while keeping other transaction's parent
+    // indexes
+  });
+  // TODO Create merge transaction
+  // Done! Change the internal format to arrays.
+  return mergee;
 }
